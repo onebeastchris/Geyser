9edb4a2a64746f8f79224359facb57c276608c5c
onebeastchris <github@onechris.mozmail.com>
2025-10-31 16:31:10 +0100

    Use nicer disconnect message to block subclients

--- core/src/main/java/org/geysermc/geyser/network/CodecProcessor.java
+++ core/src/main/java/org/geysermc/geyser/network/CodecProcessor.java
@@ -308,10 +308,10 @@ class CodecProcessor {
             .updateSerializer(CraftingEventPacket.class, ILLEGAL_SERIALIZER)
             // Illegal unusued serverbound packets that relate to unused features
             .updateSerializer(ClientCacheBlobStatusPacket.class, ILLEGAL_SERIALIZER)
-            .updateSerializer(SubClientLoginPacket.class, ILLEGAL_SERIALIZER)
             .updateSerializer(SubChunkRequestPacket.class, ILLEGAL_SERIALIZER)
             .updateSerializer(GameTestRequestPacket.class, ILLEGAL_SERIALIZER)
             // Ignored serverbound packets
+            .updateSerializer(SubClientLoginPacket.class, IGNORED_SERIALIZER)
             .updateSerializer(ClientToServerHandshakePacket.class, IGNORED_SERIALIZER)
             .updateSerializer(EntityFallPacket.class, IGNORED_SERIALIZER)
             .updateSerializer(MapCreateLockedCopyPacket.class, IGNORED_SERIALIZER)
