74a3a49cc4e3fea2fb4f68b3977dcfa100151199
chris <github@onechris.mozmail.com>
2026-01-27 00:31:48 +0100

    Disable signing for Maven publication
    Disable signing for Maven publication.

--- build-logic/src/main/kotlin/geyser.publish-conventions.gradle.kts
+++ build-logic/src/main/kotlin/geyser.publish-conventions.gradle.kts
@@ -7,3 +7,6 @@ indra {
     publishSnapshotsTo("geysermc", "https://repo.opencollab.dev/maven-snapshots")
     publishReleasesTo("geysermc", "https://repo.opencollab.dev/maven-releases")
 }
+
+val signMavenPublication by tasks
+signMavenPublication.enabled = false
